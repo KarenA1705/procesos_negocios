429a912375a4fedfddd5dd7ff94ec550443c35fc
KarenA1705 <kamontanon@ufpso.edu.co>
2022-10-27 21:25:33 -0500

    Implementacion de Spring Security y JWT

--- src/main/java/com/procesos/negocio/karen/models/Usuario.java
+++ src/main/java/com/procesos/negocio/karen/models/Usuario.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -18,13 +19,13 @@ public class Usuario {
     private Long id;
     @Column(length = 100,nullable = false)
     //@Size(min = 1,max = 100)
-    @NotBlank(message = "El campo nombre no puede estar vacio, revise los datos")
+    @NotBlank(message = "El nombre no puede estar vacio, revise los datos")
     private String nombre;
     @Column(length = 300,nullable = false)
     @NotBlank(message = "El campo apellidos no puede estar vacio, revise los datos")
     private String apellidos;
     @Column(length = 20,nullable = false,unique = true)
-    @NotBlank(message = "El campo documento no puede estar vacio, revise los datos")
+    @NotBlank(message = "El documento no puede estar vacio, revise los datos")
     private String documento;
     @Column(length = 100, nullable = true)
     private String direccion;
@@ -32,4 +33,10 @@ public class Usuario {
     private Date fechaNacimiento;
     @Column(length = 10, nullable = true)
     private String telefono;
+    @Column(length = 100,nullable = false,unique = true)
+    @NotBlank(message = "El correo no puede estar vacio, revise los datos")
+    private String correo;
+    @Column(length = 64,nullable = false)
+    @NotBlank(message = "La contraseña no puede estar vacia, revise los datos")
+    private String password;
 }
